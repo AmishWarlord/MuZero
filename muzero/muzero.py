eb05c743ba2f6b38b90369e02cf1f13263db2c76
Charles Fry <52051562+AmishWarlord@users.noreply.github.com>
2020-11-05 21:31:09 -0500

    upgraded project to latest tensorflow-gpu

--- muzero/muzero.py
+++ muzero/muzero.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('../')
+
 from config import MuZeroConfig, make_cartpole_config
 from networks.shared_storage import SharedStorage
 from self_play.self_play import run_selfplay, run_eval
